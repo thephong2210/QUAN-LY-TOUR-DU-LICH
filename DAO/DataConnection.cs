40acc79ea1650a57c35c67cd2c9c29d6bfd6f1f7
MenD <menpro147@gmail.com>
2021-10-20 21:06:00 +0700

    push

---
+++ DAO/DataConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public class DataConnection
+    {
+        SqlConnection conn;
+        private string connString = @"Data Source=LAPTOP-8JSOGGPT;Initial Catalog=tourdulich;Integrated Security=True";
+
+        public void OpenConnect()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+        }
+
+        public void CloseConnect()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            
+        }
+
+
+    }
+}
---
+++ DTO/tour.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DTO
+{
+  
+    public class tour
+    {
+        private int tenGoiTour;
+        private string dacDiem;
+        private int maDiaDiem;
+        private int maGiaTour;
+        private int maLoaiHinhDuLich;
+        private int soLuongKhachHang;
+        private double tongTien;
+        private string thoiGianBatDau;
+        private string thoiGianKetThuc;
+
+        public int TenGoiTour { get => tenGoiTour; set => tenGoiTour = value; }
+        public string DacDiem { get => dacDiem; set => dacDiem = value; }
+        public int MaDiaDiem { get => maDiaDiem; set => maDiaDiem = value; }
+        public int MaGiaTour { get => maGiaTour; set => maGiaTour = value; }
+        public int MaLoaiHinhDuLich { get => maLoaiHinhDuLich; set => maLoaiHinhDuLich = value; }
+        public int SoLuongKhachHang { get => soLuongKhachHang; set => soLuongKhachHang = value; }
+        public double TongTien { get => tongTien; set => tongTien = value; }
+        public string ThoiGianBatDau { get => thoiGianBatDau; set => thoiGianBatDau = value; }
+        public string ThoiGianKetThuc { get => thoiGianKetThuc; set => thoiGianKetThuc = value; }
+
+        public tour(int tenGoiTour, string dacDiem, int maDiaDiem, int maGiaTour, int maLoaiHinhDuLich, int soLuongKhachHang, double tongTien, string thoiGianBatDau, string thoiGianKetThuc)
+        {
+            this.TenGoiTour = tenGoiTour;
+            this.DacDiem = dacDiem;
+            this.MaDiaDiem = maDiaDiem;
+            this.MaGiaTour = maGiaTour;
+            this.MaLoaiHinhDuLich = maLoaiHinhDuLich;
+            this.SoLuongKhachHang = soLuongKhachHang;
+            this.TongTien = tongTien;
+            this.ThoiGianBatDau = thoiGianBatDau;
+            this.ThoiGianKetThuc = thoiGianKetThuc;
+        }
+    }
+}
