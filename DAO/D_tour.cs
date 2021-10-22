90a91b29b92136fd6bf03f04efeea508ffa4c100
MenD <menpro147@gmail.com>
2021-10-23 03:28:59 +0700

    Chức năng thêm tour mới

--- DAO/D_tour.cs
+++ DAO/D_tour.cs
@@ -31,20 +31,111 @@ namespace DAO
 
         }
 
-        public void ThemListTour(DTO_tour DTOtour)
+        public void ThemTour(DTO_tour DTOtour)
         {
-            //connect.OpenConnect();
-            //string querySql = "INSERT INTO tour VALUES(@)";
+            connect.OpenConnect();
+            
+            string querySql = "INSERT INTO tour VALUES(@tenGoiTour, @dacDiem, @maDiaDiem, " +
+                               "@maGiaTour, @maLoaiHinhDuLich, @soLuongKhachHang," +
+                                "@tongTien, @thoiGianBatDau, @thoiGianKetThuc)";
 
+            SqlCommand command = new SqlCommand(querySql, connect.conn);
 
+            command.Parameters.Add(new SqlParameter("@tenGoiTour", DTOtour.TenGoiTour ));
+            command.Parameters.Add(new SqlParameter("@dacDiem", DTOtour.DacDiem));
+            command.Parameters.Add(new SqlParameter("@maDiaDiem", DTOtour.MaDiaDiem));
+            command.Parameters.Add(new SqlParameter("@maGiaTour", DTOtour.MaGiaTour));
+            command.Parameters.Add(new SqlParameter("@maLoaiHinhDuLich", DTOtour.MaLoaiHinhDuLich));
+            command.Parameters.Add(new SqlParameter("@soLuongKhachHang", DTOtour.SoLuongKhachHang));
+            command.Parameters.Add(new SqlParameter("@tongTien", DTOtour.TongTien));
+            command.Parameters.Add(new SqlParameter("@thoiGianBatDau", DTOtour.ThoiGianBatDau));
+            command.Parameters.Add(new SqlParameter("@thoiGianKetThuc", DTOtour.ThoiGianKetThuc));
 
+            command.ExecuteNonQuery();
 
-            //connect.CloseConnect();
+            connect.CloseConnect();
         }
 
-        
-        
+        //ghi tạm ở DAO tour
+        public DataSet GetListDiaDiem()
+        {
+            connect.OpenConnect();
+            DataSet dataSet = new DataSet();
+            string querySql = "SELECT tenDiaDiem FROM diadiemthamquan";
 
+            SqlCommand command = new SqlCommand(querySql, connect.conn);
+
+            SqlDataAdapter adapterData = new SqlDataAdapter();
+
+            adapterData.SelectCommand = command;
+            adapterData.Fill(dataSet);
+
+            connect.CloseConnect();
+
+            return dataSet;
+        }
+
+        //tam thoi
+        public DataTable GetMaDiaDiem(string tenDiaDiem)
+        {
+            connect.OpenConnect();
+            DataTable dataSet = new DataTable();
+            string querySql = "SELECT maDiaDiem FROM diadiemthamquan WHERE tendiaDiem = N'" + tenDiaDiem + "'";
+
+       
+            System.Diagnostics.Debug.WriteLine(querySql); //viet ra console de test
+
+            SqlCommand command = new SqlCommand(querySql, connect.conn);
+
+            SqlDataAdapter adapterData = new SqlDataAdapter();
+
+            adapterData.SelectCommand = command;
+            adapterData.Fill(dataSet);
+
+            connect.CloseConnect();
+
+            return dataSet;
+        }
+
+        public DataSet GetListLoaiHinhDuLich()
+        {
+            connect.OpenConnect();
+            DataSet dataSet = new DataSet();
+            string querySql = "SELECT tenLoaiHinhDuLich FROM loaihinhdulich";
+
+            SqlCommand command = new SqlCommand(querySql, connect.conn);
+
+            SqlDataAdapter adapterData = new SqlDataAdapter();
+
+            adapterData.SelectCommand = command;
+            adapterData.Fill(dataSet);
+
+            connect.CloseConnect();
+
+            return dataSet;
+        }
+
+        //tam thoi
+        public DataTable GetMaLoaiHinhDuLich(string tenLoaiHinhDuLich)
+        {
+            connect.OpenConnect();
+            DataTable dataSet = new DataTable();
+            string querySql = "SELECT maLoaiHinhDuLich FROM loaihinhdulich WHERE tenLoaiHinhDuLich = N'" + tenLoaiHinhDuLich + "'";
+
+
+            System.Diagnostics.Debug.WriteLine(querySql); //viet ra console de test
+
+            SqlCommand command = new SqlCommand(querySql, connect.conn);
+
+            SqlDataAdapter adapterData = new SqlDataAdapter();
+
+            adapterData.SelectCommand = command;
+            adapterData.Fill(dataSet);
+
+            connect.CloseConnect();
+
+            return dataSet;
+        }
 
 
 
--- GUI/fmQuanLyTour.cs
+++ GUI/fmQuanLyTour.cs
@@ -1,4 +1,5 @@
 ﻿using BUS;
+using DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,8 +21,8 @@ namespace GUI
         {
             InitializeComponent();
             LoadDanhSachTour();
-
-
+            LoadComboboxDiaDiem();
+            LoadComboboxLoaiHinhDuLich();
 
         }
 
@@ -29,12 +30,88 @@ namespace GUI
 
         private void LoadDanhSachTour()
         {
-
             dataGridViewQuanLyTour.DataSource = b_Tour.GetListTour();
+        }
+
+        private void LoadComboboxDiaDiem()
+        {
+            comboBoxDiaDiem.DataSource = b_Tour.GetListDiaDiem().Tables[0];
+            comboBoxDiaDiem.DisplayMember = "tenDiaDiem";
+        }
+
+        private void LoadComboboxLoaiHinhDuLich()
+        {
+            comboBoxLoaiHinhDuLich.DataSource = b_Tour.GetListLoaiHinhDuLich().Tables[0];
+            comboBoxLoaiHinhDuLich.DisplayMember = "tenLoaiHinhDuLich";
+        }
+
+        private void XoaTextBox()
+        {
+            textBoxTenTour.Text = "";
+            textBoxDacDiem.Text = "";
+        }
+
+        private void ThemTour()
+        {
+
+            if (!String.IsNullOrEmpty(textBoxTenTour.Text))
+            {
+                if (!String.IsNullOrEmpty(textBoxDacDiem.Text))
+                {
+                    string tenGoi = textBoxTenTour.Text;
+                    string dacDiem = textBoxDacDiem.Text;
+                    int maDiaDiem = int.Parse(b_Tour.GetMaDiaDiem(comboBoxDiaDiem.Text).Rows[0][0].ToString());
+                    int maGiaTour = 1;
+                    int maLHDL = int.Parse(b_Tour.GetMaLoaiHinhDuLich(comboBoxLoaiHinhDuLich.Text).Rows[0][0].ToString());
+                    int soLuongKH = 0;
+                    int tongTien = 0;
+                    string thoiGianBD = dateTimePickerNgayBatDau.Value.Date.ToString("yyyy-MM-dd hh:mm:ss.ss");
+                    string thoiGianKT = dateTimePickerNgayKetThuc.Value.Date.ToString("yyyy-MM-dd hh:mm:ss.ss");
+
+                    DTO_tour DTOtour = new DTO_tour(tenGoi, dacDiem, maDiaDiem, maGiaTour, maLHDL, soLuongKH, tongTien, thoiGianBD, thoiGianKT);
+
+                    try
+                    {
+                        b_Tour.ThemTour(DTOtour);
+                        LoadDanhSachTour();
+                        XoaTextBox();
+
+                        MessageBox.Show("Thêm thành công!", "Thông báo");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Thêm không thành công!", "Thông báo");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng đặc điểm tour!", "Thông báo");
+                    textBoxDacDiem.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập tên tour!", "Thông báo");
+                textBoxTenTour.Focus();
+            }
 
             
-
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private void dataGridViewQuanLyTour_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -55,5 +132,19 @@ namespace GUI
         {
 
         }
+
+        private void buttonTaoMoi_Click(object sender, EventArgs e)
+        {
+            ThemTour();
+            
+        }
+
+        private void comboBoxDiaDiem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
+
+
+
 }
