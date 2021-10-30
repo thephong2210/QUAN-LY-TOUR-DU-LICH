6fa033ca9787d05784f3326e2efcb369258818e6
unknown <nguyenngocquynhnhu24012000@gmail.com>
2021-10-30 21:45:55 +0700

    giao diện

---
+++ GUI/LoaiHinhDuLich.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace GUI
+{
+    partial class LoaiHinhDuLich
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLoaiHinhDuLich = new System.Windows.Forms.Panel();
+            this.lbLoaiHinhDuLich = new System.Windows.Forms.Label();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.lbMaLoaiHinhDuLich = new System.Windows.Forms.Label();
+            this.lbTenLoaiHinhDuLich = new System.Windows.Forms.Label();
+            this.textBoxMaLoaiHinhDuLich = new System.Windows.Forms.TextBox();
+            this.textBoxTenLoaiHinhDuLich = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.dataGridViewLoaiHinhDuLich = new System.Windows.Forms.DataGridView();
+            this.lbTimKiem = new System.Windows.Forms.Label();
+            this.textBoxTimKiém = new System.Windows.Forms.TextBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panelLoaiHinhDuLich.SuspendLayout();
+            this.panelLeft.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLoaiHinhDuLich)).BeginInit();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLoaiHinhDuLich
+            // 
+            this.panelLoaiHinhDuLich.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panelLoaiHinhDuLich.Controls.Add(this.lbLoaiHinhDuLich);
+            this.panelLoaiHinhDuLich.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panelLoaiHinhDuLich.Location = new System.Drawing.Point(1, 3);
+            this.panelLoaiHinhDuLich.Name = "panelLoaiHinhDuLich";
+            this.panelLoaiHinhDuLich.Size = new System.Drawing.Size(1005, 86);
+            this.panelLoaiHinhDuLich.TabIndex = 0;
+            // 
+            // lbLoaiHinhDuLich
+            // 
+            this.lbLoaiHinhDuLich.AutoSize = true;
+            this.lbLoaiHinhDuLich.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbLoaiHinhDuLich.ForeColor = System.Drawing.Color.White;
+            this.lbLoaiHinhDuLich.Location = new System.Drawing.Point(344, 17);
+            this.lbLoaiHinhDuLich.Name = "lbLoaiHinhDuLich";
+            this.lbLoaiHinhDuLich.Size = new System.Drawing.Size(309, 50);
+            this.lbLoaiHinhDuLich.TabIndex = 0;
+            this.lbLoaiHinhDuLich.Text = "Loại hình du lịch";
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.panelLeft.Controls.Add(this.textBoxTenLoaiHinhDuLich);
+            this.panelLeft.Controls.Add(this.lbTenLoaiHinhDuLich);
+            this.panelLeft.Controls.Add(this.btnThem);
+            this.panelLeft.Controls.Add(this.textBoxMaLoaiHinhDuLich);
+            this.panelLeft.Controls.Add(this.lbMaLoaiHinhDuLich);
+            this.panelLeft.Location = new System.Drawing.Point(1, 155);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(417, 374);
+            this.panelLeft.TabIndex = 1;
+            // 
+            // lbMaLoaiHinhDuLich
+            // 
+            this.lbMaLoaiHinhDuLich.AutoSize = true;
+            this.lbMaLoaiHinhDuLich.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbMaLoaiHinhDuLich.Location = new System.Drawing.Point(11, 81);
+            this.lbMaLoaiHinhDuLich.Name = "lbMaLoaiHinhDuLich";
+            this.lbMaLoaiHinhDuLich.Size = new System.Drawing.Size(174, 25);
+            this.lbMaLoaiHinhDuLich.TabIndex = 2;
+            this.lbMaLoaiHinhDuLich.Text = "Mã loại hình du lịch";
+            // 
+            // lbTenLoaiHinhDuLich
+            // 
+            this.lbTenLoaiHinhDuLich.AutoSize = true;
+            this.lbTenLoaiHinhDuLich.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTenLoaiHinhDuLich.Location = new System.Drawing.Point(8, 141);
+            this.lbTenLoaiHinhDuLich.Name = "lbTenLoaiHinhDuLich";
+            this.lbTenLoaiHinhDuLich.Size = new System.Drawing.Size(177, 25);
+            this.lbTenLoaiHinhDuLich.TabIndex = 3;
+            this.lbTenLoaiHinhDuLich.Text = "Tên loại hình du lịch";
+            // 
+            // textBoxMaLoaiHinhDuLich
+            // 
+            this.textBoxMaLoaiHinhDuLich.Location = new System.Drawing.Point(210, 79);
+            this.textBoxMaLoaiHinhDuLich.Name = "textBoxMaLoaiHinhDuLich";
+            this.textBoxMaLoaiHinhDuLich.Size = new System.Drawing.Size(169, 27);
+            this.textBoxMaLoaiHinhDuLich.TabIndex = 2;
+            // 
+            // textBoxTenLoaiHinhDuLich
+            // 
+            this.textBoxTenLoaiHinhDuLich.Location = new System.Drawing.Point(210, 139);
+            this.textBoxTenLoaiHinhDuLich.Name = "textBoxTenLoaiHinhDuLich";
+            this.textBoxTenLoaiHinhDuLich.Size = new System.Drawing.Size(172, 27);
+            this.textBoxTenLoaiHinhDuLich.TabIndex = 3;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnThem.Location = new System.Drawing.Point(162, 317);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(94, 33);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnXoa.Location = new System.Drawing.Point(388, 317);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(94, 33);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSua.Location = new System.Drawing.Point(83, 317);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(94, 33);
+            this.btnSua.TabIndex = 4;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.panelRight.Controls.Add(this.btnSua);
+            this.panelRight.Controls.Add(this.btnXoa);
+            this.panelRight.Controls.Add(this.textBoxTimKiém);
+            this.panelRight.Controls.Add(this.lbTimKiem);
+            this.panelRight.Controls.Add(this.dataGridViewLoaiHinhDuLich);
+            this.panelRight.Location = new System.Drawing.Point(417, 155);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(589, 374);
+            this.panelRight.TabIndex = 2;
+            // 
+            // dataGridViewLoaiHinhDuLich
+            // 
+            this.dataGridViewLoaiHinhDuLich.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridViewLoaiHinhDuLich.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLoaiHinhDuLich.Location = new System.Drawing.Point(33, 79);
+            this.dataGridViewLoaiHinhDuLich.Name = "dataGridViewLoaiHinhDuLich";
+            this.dataGridViewLoaiHinhDuLich.RowHeadersWidth = 51;
+            this.dataGridViewLoaiHinhDuLich.RowTemplate.Height = 29;
+            this.dataGridViewLoaiHinhDuLich.Size = new System.Drawing.Size(526, 199);
+            this.dataGridViewLoaiHinhDuLich.TabIndex = 0;
+            // 
+            // lbTimKiem
+            // 
+            this.lbTimKiem.AutoSize = true;
+            this.lbTimKiem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTimKiem.Location = new System.Drawing.Point(33, 19);
+            this.lbTimKiem.Name = "lbTimKiem";
+            this.lbTimKiem.Size = new System.Drawing.Size(88, 25);
+            this.lbTimKiem.TabIndex = 5;
+            this.lbTimKiem.Text = "Tìm kiếm";
+            // 
+            // textBoxTimKiém
+            // 
+            this.textBoxTimKiém.Location = new System.Drawing.Point(167, 20);
+            this.textBoxTimKiém.Name = "textBoxTimKiém";
+            this.textBoxTimKiém.PlaceholderText = "Nhập mã hoặc tên loại hình du lịch";
+            this.textBoxTimKiém.Size = new System.Drawing.Size(285, 27);
+            this.textBoxTimKiém.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel6.Controls.Add(this.pictureBox2);
+            this.panel6.Controls.Add(this.label2);
+            this.panel6.ForeColor = System.Drawing.Color.Transparent;
+            this.panel6.Location = new System.Drawing.Point(1, 89);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(417, 68);
+            this.panel6.TabIndex = 34;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GUI.Properties.Resources.icons8_add_64;
+            this.pictureBox2.Location = new System.Drawing.Point(75, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(123, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Thêm loại hình du lịch";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel7.Controls.Add(this.pictureBox3);
+            this.panel7.Controls.Add(this.label10);
+            this.panel7.Location = new System.Drawing.Point(417, 89);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(589, 68);
+            this.panel7.TabIndex = 35;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::GUI.Properties.Resources.icons8_list_50;
+            this.pictureBox3.Location = new System.Drawing.Point(83, 10);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 31);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(150, 3);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(357, 38);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Danh sách loại hình du lịch";
+            // 
+            // LoaiHinhDuLich
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 531);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelLoaiHinhDuLich);
+            this.Name = "LoaiHinhDuLich";
+            this.Text = "Form1";
+            this.panelLoaiHinhDuLich.ResumeLayout(false);
+            this.panelLoaiHinhDuLich.PerformLayout();
+            this.panelLeft.ResumeLayout(false);
+            this.panelLeft.PerformLayout();
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLoaiHinhDuLich)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLoaiHinhDuLich;
+        private System.Windows.Forms.Label lbLoaiHinhDuLich;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.TextBox textBoxTenLoaiHinhDuLich;
+        private System.Windows.Forms.Label lbTenLoaiHinhDuLich;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox textBoxMaLoaiHinhDuLich;
+        private System.Windows.Forms.Label lbMaLoaiHinhDuLich;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.TextBox textBoxTimKiém;
+        private System.Windows.Forms.Label lbTimKiem;
+        private System.Windows.Forms.DataGridView dataGridViewLoaiHinhDuLich;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label10;
+    }
+}
